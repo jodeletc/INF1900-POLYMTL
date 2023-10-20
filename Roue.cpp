defef1968fb4f77c307d448b49d84d6068130809
samirachiny <folefacktemfacksamirachiny@gmail.com>
2023-10-20 15:54:03 -0400

    Class Roue commmencé

---
+++ Roue.cpp
@@ -0,0 +1,9 @@
+#include "Roue.h"
+#include <avr/interrupt.h>
+#include <avr/io.h
+#define F_CPU 8000000UL
+#include <util/delay.h>
+
+Roue::Roue( ) : { }
+
+Roue:: ~Roue(){ }
---
+++ Roue.h
@@ -0,0 +1,12 @@
+#include <stdint.h>
+
+Class Roue{
+    public:
+    Roue(  );
+    ~Roue();
+
+     
+
+    private:
+
+};
